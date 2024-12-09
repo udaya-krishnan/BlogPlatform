a97f289228b06f36cafd83278bb0f77ce73d9364
udaya-krishnan <udayankrishnan36@gmail.com>
2024-12-09 10:13:08 +0530

    url change

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: ["http://localhost:5173",], 
+    origin: ["http://localhost:5173","https://blog-platform-delta-six.vercel.app"], 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
